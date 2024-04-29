a1fad736196ad68f3eccb2459a823471be61e551
Aleksander <scrawach@gmail.com>
2024-04-29 22:03:00 +0300

    added evaluation nodes for tokens

--- LogicalExpressionParser/LineOfTokens.cs
+++ LogicalExpressionParser/LineOfTokens.cs
@@ -3,21 +3,82 @@ using System.Text.RegularExpressions;
 
 namespace LogicalExpressionParser;
 
-public class LineOfTokens : IEnumerable<Token>
+public class TokenFactory
 {
-    private readonly Regex _regex = new Regex(@"([\w]+|[><=&+!|]|=>)\s*");
-    private readonly string _inputLine;
+    public Token Parse(string content)
+    {
+        return content.ToLower() switch
+        {
+            "|" => new OperatorToken(1),
+            "&" => new OperatorToken(1),
+            ">" => new MoreToken(2),
+            "<" => new OperatorToken(2),
+            "=" => new OperatorToken(3),
+            _ => new VariableToken(content)
+        };
+    }
+}
 
-    public LineOfTokens(string inputLine) => 
-        _inputLine = inputLine;
+public class Test
+{
+    public void Usage()
+    {
+         var expression = new LogicalExpression
+         (
+             new PostfixTokens
+             (
+                 new LineOfTokens
+                 (
+                     new ParsedString("1 + 2"), 
+                     new TokenFactory()
+                 )
+             )
+         ); 
+         
+         expression.Evaluate();
+    }
+}
 
-    public IEnumerator<Token> GetEnumerator()
+public class LogicalExpression
+{
+    private readonly PostfixTokens _tokens;
+
+    public LogicalExpression(PostfixTokens tokens) => 
+        _tokens = tokens;
+
+    public bool Evaluate()
+    {
+        var operands = new Stack<VariableToken>();
+        
+        foreach (var token in _tokens)
+        {
+            switch (token)
+            {
+                case VariableToken:
+                    break;
+                
+                case OperatorToken:
+                    break;
+            }
+        }
+    }
+}
+
+public class ParsedString : IEnumerable<string>
+{
+    private readonly Regex _regex = new Regex(@"([\w]+|[><=&+!|]+|=>)\s*");
+    private readonly string _input;
+
+    public ParsedString(string input) => 
+        _input = input;
+
+    public IEnumerator<string> GetEnumerator()
     {
         var position = 0;
         
-        while (position < _inputLine.Length)
+        while (position < _input.Length)
         {
-            var match = _regex.Match(_inputLine, position);
+            var match = _regex.Match(_input, position);
 
             if (!match.Success)
                 throw new Exception($"Invalid token at position {position}");
@@ -25,17 +86,29 @@ public class LineOfTokens : IEnumerable<Token>
             var token = match.Groups[1].Value;
             position += match.Length;
 
-            yield return token.ToLower() switch
-            {
-                "|" => new OperatorToken(1),
-                "&" => new OperatorToken(1),
-                ">" => new MoreToken(2),
-                "<" => new OperatorToken(2),
-                "=" => new OperatorToken(3),
-                _ => new VariableToken(token)
-            };
+            yield return token;
         }
     }
+
+    IEnumerator IEnumerable.GetEnumerator() => 
+        GetEnumerator();
+}
+
+public class LineOfTokens : IEnumerable<Token>
+{
+    private readonly ParsedString _parsedString;
+    private readonly TokenFactory _tokenFactory;
+
+    public LineOfTokens(ParsedString parsedString, TokenFactory tokenFactory)
+    {
+        _parsedString = parsedString;
+        _tokenFactory = tokenFactory;
+    }
+
+    public IEnumerator<Token> GetEnumerator() => 
+        _parsedString
+            .Select(parsedString => _tokenFactory.Parse(parsedString))
+            .GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator() => 
         GetEnumerator();
--- LogicalExpressionParser/Token.cs
+++ LogicalExpressionParser/Token.cs
@@ -1,6 +1,129 @@
 ﻿namespace LogicalExpressionParser;
 
-public class Token
+public abstract class Token
 {
-    
+}
+
+public interface IUnaryOperationToken
+{
+    INode Create(INode argument);
+}
+
+public interface IBinaryOperationToken
+{
+    INode Create(INode left, INode right);
+}
+
+public interface IToken
+{
+
+}
+
+public class VariableNode : INode
+{
+    private readonly VariableToken _token;
+
+    public VariableNode(VariableToken token) => 
+        _token = token;
+
+    public int Evaluate(IVariables variables) => 
+        variables[_token.Name];
+}
+
+public interface INode
+{
+    int Evaluate(IVariables variables);
+}
+
+public interface IVariables
+{
+    int this[string variableName] { get; }
+}
+
+public class NotOperatorNode : INode
+{
+    private readonly string _variableName;
+
+    public NotOperatorNode(string variableName) => 
+        _variableName = variableName;
+
+    public int Evaluate(IVariables variables)
+    {
+        var value = variables[_variableName];
+        return value > 0 ? 0 : 1;
+    }
+}
+
+public class AddOperatorNode : INode
+{
+    private readonly INode _left;
+    private readonly INode _right;
+
+    public AddOperatorNode(INode left, INode right)
+    {
+        _left = left;
+        _right = right;
+    }
+
+    public int Evaluate(IVariables variables) => 
+        _left.Evaluate(variables) + _right.Evaluate(variables);
+}
+
+public class SubtractOperatorNode : INode
+{
+    private readonly INode _left;
+    private readonly INode _right;
+
+    public SubtractOperatorNode(INode left, INode right)
+    {
+        _left = left;
+        _right = right;
+    }
+    public int Evaluate(IVariables variables) => 
+        _left.Evaluate(variables) - _right.Evaluate(variables);
+}
+
+
+public abstract class BooleanOperator : INode
+{
+    private readonly INode _left;
+    private readonly INode _right;
+
+    protected BooleanOperator(INode left, INode right)
+    {
+        _left = left;
+        _right = right;
+    }
+
+    public int Evaluate(IVariables variables) => 
+        Evaluate(_left.Evaluate(variables), _right.Evaluate(variables)) ? 1 : 0;
+
+    protected abstract bool Evaluate(int left, int right);
+}
+
+public class LessOperatorNode : BooleanOperator
+{
+    public LessOperatorNode(INode left, INode right) 
+        : base(left, right) { }
+
+    protected override bool Evaluate(int left, int right) => 
+        left < right;
+}
+
+public class GreaterOperatorNode : BooleanOperator
+{
+    public GreaterOperatorNode(INode left, INode right) 
+        : base(left, right) { }
+
+    protected override bool Evaluate(int left, int right) => 
+        left > right;
+}
+
+public class EqualOperatorNode : BooleanOperator
+{
+    public EqualOperatorNode(INode left, INode right) 
+        : base(left, right) { }
+
+    protected override bool Evaluate(int left, int right) => 
+        left == right;
 }
